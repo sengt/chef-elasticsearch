e68153a9a39cb28bf30586b6d0688dae73f1b32b
Seng Tang <engineers+stang@sharethrough.com>
2014-10-15 09:11:51 -0700

    update

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 #-*- encoding : utf-8 -*-
-name             "elasticsearch"
-maintainer       "Ian Ehlert"
-maintainer_email "ian.ehlert@sportngin.com"
-license          "Apache"
-description      "Installs elasticsearch"
+name             'elasticsearch'
+maintainer       'Ian Ehlert'
+maintainer_email 'ian.ehlert@sportngin.com'
+license          'Apache'
+description      'Installs elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.1.0"
+version          '0.1.0'
 
-depends "java"
+depends 'java'
